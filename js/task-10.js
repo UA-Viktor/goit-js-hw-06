a1f6ff83b3466dc8be258a02490e82da6ace50c9
UA-Viktor <v.korenkov@icloud.com>
2023-01-26 19:57:22 +0200

    uraaa!!

--- js/task-10.js
+++ js/task-10.js
@@ -1,64 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает 
-// кнопку Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число.Функция создает столько < div >,
-// сколько указано в amount и добавляет их в div#boxes.
-
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. 
-// Используй готовую функцию getRandomHexColor для получения цвета.
-
-const createBtn = document.querySelector('[data-create]');
-
-const destroyBtn = document.querySelector('[data-destroy]');
+};
 
 const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxContainer = document.querySelector('#boxes');
 
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-createBtn.addEventListener('click', )
+function createBoxes(event) {
+  for (let index = 0; index < inputEl.value; index += 1) {
 
-console.log(inputEl.value);
+    const div = document.createElement('div');
+    div.style.width = `${30 + index * 10}px`;
+    div.style.height = `${30 + index * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
 
-function createBoxes(amount) {
+    boxContainer.append(div);
+  };
+};
 
-}
-
-
-const colorPickerContainerEl = document.querySelector('#boxes');
-
-// const elements = colorPickerOptions.map(option => {
-//   const buttonEl = document.createElement('button');
-//   buttonEl.type = 'button';
-//   buttonEl.classList.add('color-picker__option');
-//   buttonEl.textContent = option.label;
-//   buttonEl.style.backgroundColor = option.color;
-
-//   return buttonEl;
-// });
-
-// console.log(elements);
-
-/*
- * Пишем функцию для создания разметки колорпикера
- */
-
-// const makeColorPickerOptions = options => {
-//   return options.map(option => {
-//     const buttonEl = document.createElement('button');
-//     buttonEl.type = 'button';
-//     buttonEl.classList.add('color-picker__option');
-//     buttonEl.textContent = option.label;
-//     buttonEl.style.backgroundColor = option.color;
-
-//     return buttonEl;
-//   });
-// };
-
-// const elements = makeColorPickerOptions(colorPickerOptions);
-// colorPickerContainerEl.append(...elements);
+function destroyBoxes(event) {
+  boxContainer.remove('div');
+};
